c08c3cc435df45a3fc61560d6820f0f1830461e9
Lucas Barbosa <eu@luccasbarbosa.com.br>
2020-10-20 00:56:29 -0300

    Commit davinday

--- public/js/app.js
+++ public/js/app.js
@@ -49886,8 +49886,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Library/WebServer/Documents/davinday/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Library/WebServer/Documents/davinday/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Library/WebServer/Documents/davinday2/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Library/WebServer/Documents/davinday2/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
